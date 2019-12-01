aa1960e9d13ee6e12386e949bdd7a0b5d5a28cb6
Vladimír Macháček <8machy@seznam.cz>
2019-12-01 10:12:43 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,8 +27,14 @@ class Configuration implements ConfigurationInterface
 	 */
 	public function getConfigTreeBuilder() : TreeBuilder
 	{
-		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root('web_loader');
+		$treeBuilder = new TreeBuilder('webloader');
+		if (method_exists($treeBuilder, 'getRootNode')) {
+		    $rootNode = $treeBuilder->getRootNode();
+
+		} else {
+		    // BC layer for symfony/config 4.1 and older
+		    $rootNode = $treeBuilder->root('web_loader');
+		}
 
 		$rootNode
 			->children()
